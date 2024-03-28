2f8a49230548e748dbb7bf79c3a3aaa4b621e13a
Jason Johnson <Jason.Johnson@devops.ncia.nato.int>
2024-03-28 15:37:16 +0000

    pass PAT to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,4 +4,5 @@ terraform {
 
 provider "azuredevops" {
   org_service_url = "https://dev.azure.com/jasojohnson"
+  personal_access_token = var.AZURE_DEVOPS_PAT
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,7 @@ variable "location" {
   description = "default location to use if not specified"
   default = "westeurope"  
 }
+
+variable "AZURE_DEVOPS_PAT" {
+  description = "Azure DevOps Personal Access Token" 
+}
